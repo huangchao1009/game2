6fbcbad8f0bb7d14d484d4ec564ac2c624a67df9
hc <443090212@qq.com>
2018-04-16 23:07:56 +0800

    upadate XGb

--- XGboost.py
+++ XGboost.py
@@ -10,6 +10,8 @@ train_data=pd.read_csv('./latest_data/train_data.csv')
 train_data=train_data.astype('double')
 test_data=pd.read_csv('./latest_data/test_data.csv')
 test_data=test_data.astype('double')
+
+
 label1=pd.read_csv('./latest_data/label1.csv')
 label1=label1['Ki']
 label2=pd.read_csv('./latest_data/label2.csv')
@@ -21,6 +23,7 @@ x_train,x_val,y_train,y_val=train_test_split(train_data,label1,test_size=0.2,ran
 # test=lgb.Dataset(test_data,label=label2,reference=train)
 xgb_train=xgb.DMatrix(x_train,label=y_train)
 xgb_val=xgb.DMatrix(x_val,label=y_val)
+xgb_test=xgb.DMatrix(test_data)
 print('使用XGboost训练')
 
 params={
@@ -41,19 +44,19 @@ params={
 #plst+=[('eval_metric','auc')]
 #evallist=[(x_val,'eval'),(x_train,'train')]
 
-num_round=1000
+num_round=3000
 plst=list(params.items())
 plst+= [('eval_metric', 'auc')]
 evallist = [(xgb_val, 'eval'), (xgb_train, 'train')]
 xgb=xgb.train(params,xgb_train,num_boost_round=num_round)
 print("save model")
-xgb.save_model('./model/model3.txt')
+xgb.save_model('./model/model4.txt')
 
 print('开始预测')
-preds_sub=xgb.predict(test_data)
+preds_sub=xgb.predict(xgb_test)
 
 test_tobepredicted['Ki']=preds_sub
-test_tobepredicted.to_csv('./result/result3.csv',index=False)
+test_tobepredicted.to_csv('./result/result4.csv',index=False)
 
 # with open("result1.csv", "w") as f:
 #     sys.stdout = f
